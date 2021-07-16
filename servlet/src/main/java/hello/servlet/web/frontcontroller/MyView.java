24a3b777f1db56bbcc9a0a6c6021c0599d613e0d
0923kdh <0923kdh@gmail.com>
2021-07-16 22:12:44 +0900

    #17. Model 추가 - Controller에서 서블릿 종속성 제거하여 구현. - view 중복 이름 제거하고 논리이름만 반환되도록 구현.

--- servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/MyView.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
 public class MyView {
 
@@ -20,4 +21,23 @@ public class MyView {
         dispatcher.forward(request, response);
 
     }
+
+    public void render(Map<String, Object> model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        modelToRequestAttribute(model, request);
+
+        //jsp forward를 하면 jsp가 request.getAttribute를 한다.
+        RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
+        dispatcher.forward(request, response);
+
+    }
+
+    private void modelToRequestAttribute(Map<String, Object> model, HttpServletRequest request) {
+
+        //jsp는 request.getAttribute로 데이터를 조회하기 때문에
+        //모델에 있는 데이터를 다 꺼내서 request에 setAttribute를 이용해서 정보를 다 담는다.
+        model.forEach((key, value)-> request.setAttribute(key, value));
+
+    }
+
 }
---
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v3/FrontControllerServletV3.java
@@ -0,0 +1,74 @@
+package hello.servlet.web.frontcontroller.v3;
+
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "frontControllerServletV3", urlPatterns = "/front-controller/v3/*")
+public class FrontControllerServletV3 extends HttpServlet {
+
+    private Map<String, ControllerV3> controllerMap = new HashMap<>();
+
+    public FrontControllerServletV3() {
+
+        controllerMap.put("/front-controller/v3/members/new-form", new MemberFormControllerV3());
+        controllerMap.put("/front-controller/v3/members/save", new MemberSaveControllerV3());
+        controllerMap.put("/front-controller/v3/members", new MemberListControllerV3());
+
+    }
+
+    @Override //1.reuquest 요청이 들어옴.
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        //getRequestURI를 하면 /front-controller/v3/* 에 해당하는 정보가 들어옴
+        String requestURI = request.getRequestURI();
+
+        //2. Controller 조회
+        ControllerV3 controller = controllerMap.get(requestURI);
+        if(controller == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        //3. Controller 호출
+        Map<String, String> paramMap = creatParamMap(request);
+        ModelView mv = controller.process(paramMap);
+
+        //4. Controller는 FrontController한테 viewName 반환
+        String viewName = mv.getViewName(); //new-from 같은 논리이름임.
+
+        //6. viewResolver가 FrontController한테 MyView 반환
+        MyView view = viewResolver(viewName);
+
+        //7. render() 호출
+        view.render(mv.getModel(), request, response);
+    }
+
+    private MyView viewResolver(String viewName) {
+
+        //5. FrontController가 viewResolver 호출
+        return new MyView("/WEB-INF/views/" + viewName + ".jsp");
+
+    }
+
+    private Map<String, String> creatParamMap(HttpServletRequest request) {
+
+        Map<String, String> paramMap = new HashMap<>();
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName->paramMap.put(paramName, request.getParameter(paramName)));
+        return paramMap;
+
+    }
+
+}
